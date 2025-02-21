4342f6a2fdc87ea8e0fb195c73b36acb5023c0ec
Idris Hanafi <ifh101@gmail.com>
2025-02-20 23:28:41 -0500

    feat: register agent callback URL

--- lib/hedge_fund_interview/beams/begin_interview.ex
+++ lib/hedge_fund_interview/beams/begin_interview.ex
@@ -1,5 +1,7 @@
 defmodule HedgeFundInterview.Beams.BeginInterview do
   alias HedgeFundInterview.Prisms.SendResponseSignal
+  alias HedgeFundInterview.Prisms.RegisterAgentSignal
+
   use Lux.Beam,
     name: "Begin Interview",
     description: "A workflow for beginning an interview",
@@ -8,6 +10,7 @@ defmodule HedgeFundInterview.Beams.BeginInterview do
   @impl true
   def steps do
     sequence do
+      step(:register_agent, RegisterAgentSignal, [:input])
       step(:send_response, SendResponseSignal, [:input])
     end
   end
