d5e4e96a01d4ee4e4c866547ca4174ab77509f1c
Zbigniew Pekala <z@spectrallabs.xyz>
2025-02-22 07:09:12 +0100

    Add /api/signals path to AGENT_CALLBACK_URL"

--- lib/hedge_fund_interview/prisms/register_agent_signal.ex
+++ lib/hedge_fund_interview/prisms/register_agent_signal.ex
@@ -4,10 +4,12 @@ defmodule HedgeFundInterview.Prisms.RegisterAgentSignal do
   @register_agent_schema_id "70112206-2558-4ff3-9f06-f82dce525a79"
 
   def handler(_, _ctx) do
+    agent_address = Path.join(System.get_env("AGENT_CALLBACK_URL"), "api/signals")
+
     register_agent_signal = %{
       id: Lux.UUID.generate(),
       payload: %{
-        agent_address: System.get_env("AGENT_CALLBACK_URL")
+        agent_address: agent_address
       },
       sender: System.get_env("ANS_HANDLE"),
       receiver: "spectra_ceo",
