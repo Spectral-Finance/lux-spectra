18af984f653b6dd76216deccd88d7e5bca42abcb
Ian <ian@spectrallabs.xyz>
2025-02-23 20:23:29 +0100

    Add auth header when sending signals

--- lib/hedge_fund_interview/prisms/register_agent_signal.ex
+++ lib/hedge_fund_interview/prisms/register_agent_signal.ex
@@ -1,6 +1,8 @@
 defmodule HedgeFundInterview.Prisms.RegisterAgentSignal do
   use Lux.Prism
 
+  alias HedgeFundInterview.Prisms.Utils
+
   @register_agent_schema_id "70112206-2558-4ff3-9f06-f82dce525a79"
 
   def handler(_, _ctx) do
@@ -18,21 +20,12 @@ defmodule HedgeFundInterview.Prisms.RegisterAgentSignal do
       signal_schema_id: @register_agent_schema_id
     }
 
-    case send_signal(register_agent_signal) do
+    case Utils.send_signal(register_agent_signal) do
       {:ok, request_response} ->
         {:ok, request_response}
 
       {:error, error} ->
         {:error, error}
-    end
-  end
-
-  defp send_signal(signal) do
-    endpoint = System.get_env("SPECTRAL_API_AGENTS_ENDPOINT")
-
-    case Req.post("#{endpoint}/signals/register", json: signal) do
-      {:ok, response} -> {:ok, response}
-      {:error, error} -> {:error, error}
     end
   end
 end
--- lib/hedge_fund_interview/prisms/utils.ex
+++ lib/hedge_fund_interview/prisms/utils.ex
@@ -1,8 +1,12 @@
 defmodule HedgeFundInterview.Prisms.Utils do
   def send_signal(signal) do
     endpoint = System.get_env("SPECTRAL_API_AGENTS_ENDPOINT")
+    api_key = System.get_env("SPECTRAL_API_KEY")
 
-    case Req.post("#{endpoint}/signals/register", json: signal) do
+    case Req.post("#{endpoint}/signals/register",
+           json: signal,
+           headers: [{"authorization", "Bearer #{api_key}"}]
+         ) do
       {:ok, response} -> {:ok, response}
       {:error, error} -> {:error, error}
     end
