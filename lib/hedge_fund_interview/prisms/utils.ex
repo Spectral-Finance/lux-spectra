76a3c5e010c002b747a37d2fb3338d551f582329
Ian <ian@spectrallabs.xyz>
2025-02-28 14:55:02 +0100

    Use module attribute to store the agents API url instead of an envar

--- lib/hedge_fund_interview/prisms/utils.ex
+++ lib/hedge_fund_interview/prisms/utils.ex
@@ -1,10 +1,9 @@
 defmodule HedgeFundInterview.Prisms.Utils do
-  @agent_api_endpoint "https://agents-api.spectrallabs.xyz/"
+  @spectral_agents_api_url "https://agents-api.spectrallabs.xyz/"
 
   def send_signal(signal) do
-    endpoint = System.get_env("SPECTRAL_API_AGENTS_ENDPOINT") || @agent_api_endpoint
     auth = {:bearer, System.get_env("SPECTRAL_API_KEY")}
 
-    Req.post("#{endpoint}/signals/register", json: signal, auth: auth)
+    Req.post("#{@spectral_agents_api_url}/signals/register", json: signal, auth: auth)
   end
 end
