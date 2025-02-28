307fea1c77269c11fe84ab1d46f33fa209836fb8
Ian <ian@spectrallabs.xyz>
2025-02-28 11:42:39 +0100

    Use default agents api endpoint overridable by envar

--- lib/hedge_fund_interview/prisms/utils.ex
+++ lib/hedge_fund_interview/prisms/utils.ex
@@ -1,6 +1,8 @@
 defmodule HedgeFundInterview.Prisms.Utils do
+  @agent_api_endpoint "https://agents-api.spectrallabs.xyz/"
+
   def send_signal(signal) do
-    endpoint = System.get_env("SPECTRAL_API_AGENTS_ENDPOINT")
+    endpoint = System.get_env("SPECTRAL_API_AGENTS_ENDPOINT") || @agent_api_endpoint
     auth = {:bearer, System.get_env("SPECTRAL_API_KEY")}
 
     Req.post("#{endpoint}/signals/register", json: signal, auth: auth)
