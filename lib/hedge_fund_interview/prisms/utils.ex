9fb7ed12fb9ea1ca2f56ca17533a6cbb46b16ed1
Ian González <ian@spectrallabs.xyz>
2025-03-04 20:01:25 +0100

    Use default agents api endpoint overridable by envar (#19)
    * Use default agents api endpoint overridable by envar
    
    * Use module attribute to store the agents API url instead of an envar

--- lib/hedge_fund_interview/prisms/utils.ex
+++ lib/hedge_fund_interview/prisms/utils.ex
@@ -1,8 +1,9 @@
 defmodule HedgeFundInterview.Prisms.Utils do
+  @spectral_agents_api_url "https://agents-api.spectrallabs.xyz/"
+
   def send_signal(signal) do
-    endpoint = System.get_env("SPECTRAL_API_AGENTS_ENDPOINT")
     auth = {:bearer, System.get_env("SPECTRAL_API_KEY")}
 
-    Req.post("#{endpoint}/signals/register", json: signal, auth: auth)
+    Req.post("#{@spectral_agents_api_url}/signals/register", json: signal, auth: auth)
   end
 end
